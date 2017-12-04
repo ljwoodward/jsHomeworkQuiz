b8905e6cf80ef23c136a229e3183ad5fb9eece6f
Laurence <ljwoodward.code@gmail.com>
2017-12-04 21:15:19 +0000

    done episodes 1-6

---
+++ homework.js
@@ -0,0 +1,106 @@
+// JS Scope Homework
+//
+// Go through each sample code and work out what the output will be and explain what happened.
+//
+// Episode 1
+//
+var name = 'Keith';
+
+var printName = function() {
+  console.log('My name is ' + name );
+};
+
+printName();
+
+--> "My name is Keith"
+
+// Episode 2
+//
+score = 5;
+
+var result = function() {
+  var score = 3;
+  return score;
+};
+
+console.log(result());
+
+--> 3
+
+// Episode 3
+//
+var myAnimals = ['Chickens', 'Cats', 'Rabbits'];
+
+var listAnimals = function() {
+  myAnimals = ['Ducks', 'Dogs', 'Lions'];
+  for(var i=0;i<myAnimals.length; i++){
+    console.log(i + ": " + myAnimals[i]);
+  }
+}
+
+listAnimals();
+
+--> 1: "Ducks"
+    2: "Dogs"
+    3: "Lions"
+
+// Episode 4
+//
+var suspectOne = 'Jay';
+var suspectTwo = 'Val';
+var suspectThree = 'Keith';
+var suspectFour = 'Rick';
+
+var allSuspects = function() {
+  var suspectThree = 'Harvey'
+  console.log('Suspects include: ' + suspectOne + ', ' + suspectTwo + ', ' + suspectThree + ', ' + suspectFour)
+};
+
+allSuspects();
+console.log( 'Suspect three is:' + suspectThree );
+
+--> "Suspects include: Jay, Val, Harvey, Rick"
+    "Suspect three is Harvey"
+
+// Episode 5
+//
+var detective = {
+  name : 'Ace Ventura',
+  pet : 'monkey'
+};
+
+var printName = function(detective) {
+  return detective.name
+};
+
+var detectiveInfo = function() {
+  detective['name'] = 'Poirot'
+  return printName(detective);
+};
+
+console.log(detectiveInfo());
+
+--> 'Poirot'
+
+// Episode 6
+//
+var murderer = 'rick';
+
+var outerFunction = function() {
+  var murderer = 'marc';
+
+  var innerFunction = function() {
+    murderer = 'valerie';
+  }
+
+  innerFunction();
+}
+
+outerFunction();
+console.log('the murderer is ', murderer);
+
+--> "the murderer is valerie"
+
+// Episode 7 - Make up your own episode/s!
+//
+// Make up your own episode which can be whatever you wish and the rest of the class will work out together what happened and what the output will be.
